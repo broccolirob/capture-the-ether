1befc7178cb51a8c3d01a985b05382f6cf5f8a79
broccolirob <admin@dfsnft.com>
2021-11-05 18:08:11 -0400

    guess random num solution.

--- scripts/guess-random-number.js
+++ scripts/guess-random-number.js
@@ -1,10 +1,25 @@
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
-  const randomNumber = await hre.ethers.getContractAt(
+  const randomNumber = await ethers.getContractAt(
     "GuessTheRandomNumberChallenge",
     "0x67efAE83aE60607AF787e53915fBB05AcD5A8d9E"
   );
+
+  // Potential Alternative: see if theres a way to convert hash bytes to uint8
+  // using ethers?
+  // const deployBlock = 11368120;
+  // const block = await ethers.provider.getBlock(deployBlock);
+  // const answerBytes = ethers.utils.keccak256(block.parentHash, block.timestamp);
+  // const [answer] = ethers.utils.defaultAbiCoder.decode(["uint8"], answerBytes);
+
+  const storedNum = await ethers.provider.getStorageAt(randomNumber.address, 0);
+
+  await randomNumber
+    .guess(storedNum, {
+      value: ethers.utils.parseEther("1"),
+    })
+    .then((tx) => tx.wait());
 
   console.log("isComplete:", await randomNumber.isComplete());
 }
